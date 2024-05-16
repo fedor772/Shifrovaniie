5331bcbc80244ce8003ee735dab571f8cf29e4e5
fedor772 <160321057+fedor772@users.noreply.github.com>
2024-05-16 17:01:57 +0000

    Добавление возможности расшифровки текста

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,74 +1,39 @@
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
 import java.util.Scanner;
 import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        // Чтение файла
-        String fileName = "text.txt";
-        StringBuilder fileContent = new StringBuilder();
-        String str = "";
-        String key = "";
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                fileContent.append(line).append("\n");
-            }
-        } catch (IOException e) {
-            System.err.println("Ошибка чтения файла: " + e.getMessage());
-        }
-        // Шифрование
-        try {
-            KeyGenerator keygenerator = KeyGenerator.getInstance("DES");
-            SecretKey myDesKey = keygenerator.generateKey();
-            key = myDesKey.getEncoded().toString();
-            Cipher desCipher;
-            desCipher = Cipher.getInstance("DES");
-            byte[] text = fileContent.toString().getBytes("UTF8");
-
-            desCipher.init(Cipher.ENCRYPT_MODE, myDesKey);
-            byte[] textEncrypted = desCipher.doFinal(text);
-            System.out.print("Зашифрованный текст: ");
-            String s = new String(textEncrypted);
-            System.out.println(s);
-            str = s;
-            desCipher.init(Cipher.DECRYPT_MODE, myDesKey);
-            byte[] textDecrypted = desCipher.doFinal(textEncrypted);
-            System.out.print("Исходный текст: ");
-            s = new String(textDecrypted);
-            System.out.println(s);
-        } catch (Exception e) {
-            System.out.println("Ошибка");
-        }
-        System.out.println("Ключ шифрования: " + key);
-        // Запись зашифрованного текста в файл
-        PrintWriter writer = null;
-        try {
-            writer = new PrintWriter("encrypted.txt");
-            writer.print(str);
-        } catch (IOException e) {
-            System.err.println("Ошибка записи в файл: " + e.getMessage());
-        } finally {
-            if (writer != null) {
-                writer.close();
-            }
-        }
-        //Запись ключа в файл
-        writer = null;
-        try {
-            writer = new PrintWriter("key.txt");
-            writer.print(key);
-        } catch (IOException e) {
-            System.err.println("Ошибка записи в файл: " + e.getMessage());
-        } finally {
-            if (writer != null) {
-                writer.close();
-            }
-        }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Режимы работы: 0 - шифрование, 1 - расшифровка");
+    System.out.print("Выберите режим работы: ");
+    int mode = sc.nextInt();
+    switch (mode) {
+      case 0:
+        System.out.println("Загрузка...");
+        comm("clear");
+        comm("java -classpath .:target/dependency/* Crypt");
+        break;
+      case 1:
+        System.out.println("Загрузка...");
+        comm("clear");
+        comm("java -classpath .:target/dependency/* Encrypt");
+        break;
     }
+  }
+
+  public static void comm(String prc) {
+    try {
+      Process process = Runtime.getRuntime().exec(prc);
+      BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+      String line;
+      while ((line = reader.readLine()) != null) {
+        System.out.println(line);
+      }
+      process.waitFor();
+      reader.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
